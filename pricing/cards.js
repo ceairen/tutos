32fa09a840c2539291dc572193c1cc4012fa2b4b
Yoann BRAIE <yoannbraie@MCBKY.local>
2024-06-07 11:09:27 +0200

    feat [tuto]: css pricing

---
+++ pricing/cards.js
@@ -0,0 +1,31 @@
+export const getCards = () => [
+  {
+    title: "Starter",
+    price: {
+      monthly: "0,99",
+      yearly: "9,99",
+    },
+    features: [
+      {
+        title: "feature 1",
+        available: true,
+      },
+      {
+        title: "feature 2",
+        available: true,
+      },
+      {
+        title: "feature 3",
+        available: false,
+      },
+      {
+        title: "feature 4",
+        available: false,
+      },
+      {
+        title: "feature 5",
+        available: false,
+      },
+    ],
+  },
+];
